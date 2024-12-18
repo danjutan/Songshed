e6c58d8c6db0ff0e5b6f8a9155710dadbe7d96ad
danjutan <danjutan@gmail.com>
2024-12-18 03:27:20 +0000

    commit so I can switch IDEs

---
+++ components/tab/guitar/overlay/OverlaySVG.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+import {
+  ResizeStateInjectionKey,
+  type ResizeState,
+} from "../../state/resize-state";
+
+const resizeState = inject(ResizeStateInjectionKey) as ResizeState;
+
+const lineStart = computed(
+  () => resizeState.getStackCoords(resizeState.subUnit.value * 2).value?.center,
+);
+const lineEnd = computed(
+  () => resizeState.getStackCoords(resizeState.subUnit.value * 4).value?.center,
+);
+</script>
+<template>
+  <ClientOnly>
+    <svg>
+      <line :x1="lineStart" :x2="lineEnd" :y1="20" :y2="20" />
+    </svg>
+  </ClientOnly>
+</template>
+
+<style scoped>
+svg {
+  width: 100%;
+  height: 100%;
+}
+
+line {
+  stroke-width: 10px;
+  stroke: black;
+}
+</style>
