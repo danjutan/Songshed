6d01f70338d2751330f0865272521a9251d5b358
spatterspace <me@spatter.space>
2024-12-23 08:44:11 -0500

    basic slide

--- components/tab/guitar/overlay/tie/TieRender.vue
+++ components/tab/guitar/overlay/tie/TieRender.vue
@@ -6,6 +6,22 @@ import OverlayCoords from "../OverlayCoords.vue";
 const props = withDefaults(defineProps<{ tie: Tie; firstRow: number }>(), {
   firstRow: 1,
 });
+
+const connected = computed(
+  () => props.tie.midiFrom !== undefined && props.tie.midiTo !== undefined,
+);
+
+const ascending = computed(
+  () => !connected.value || props.tie.midiFrom! < props.tie.midiTo!,
+);
+const row = computed(() => props.firstRow + props.tie.string - 1);
+
+const slideRowStart = computed(() =>
+  ascending.value ? row.value + 0.2 : row.value + 0.8,
+);
+const slideRowEnd = computed(() =>
+  ascending.value ? row.value + 0.8 : row.value + 0.2,
+);
 </script>
 
 <template>
@@ -15,6 +31,7 @@ const props = withDefaults(defineProps<{ tie: Tie; firstRow: number }>(), {
   >
     <svg v-if="from && to">
       <path
+        v-if="tie.type.hammer"
         :d="
           useTiePath(
             (from.center + from.right) / 2,
@@ -23,12 +40,20 @@ const props = withDefaults(defineProps<{ tie: Tie; firstRow: number }>(), {
           )
         "
       />
+      <line
+        v-if="tie.type.slide"
+        :x1="from.center + (from.right - from.left) * 0.4"
+        :x2="to.center - (to.right - to.left) * 0.4"
+        :y1="slideRowStart * cellHeight"
+        :y2="slideRowEnd * cellHeight"
+      />
     </svg>
   </OverlayCoords>
 </template>
 
 <style scoped>
-path {
+path,
+line {
   stroke: black;
   stroke-width: 1;
 }
