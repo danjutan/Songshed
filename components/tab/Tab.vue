ea9aa2563e7c364e4303e62f65c24beeef02cb7e
spatterspace <me@spatter.space>
2025-02-17 20:36:14 -0500

    hoist css variables to editor-app

--- components/tab/Tab.vue
+++ components/tab/Tab.vue
@@ -39,10 +39,6 @@ const props = defineProps<{
 }>();
 
 const settings = injectSettingsState();
-const cellHeightPx = computed(() => `${settings.cellHeight}px`);
-const dividerWidthPx = computed(() => `${settings.dividerWidth}px`);
-const contextMenuHeightPx = computed(() => `${settings.contextMenuHeight}px`);
-const collapsedMinWidthPx = computed(() => `${settings.collapsedMinWidth}px`);
 
 const subUnit = provideSubUnit(props.tabStore, settings);
 const beatSize = provideBeatSize(props.tabStore);
@@ -272,100 +268,9 @@ const deletingBarStart = ref<number | undefined>(undefined);
 
 <style scoped>
 .tab {
-  --cell-height: v-bind(cellHeightPx);
-  --context-menu-height: v-bind(contextMenuHeightPx);
-  --collapsed-min-width: v-bind(collapsedMinWidthPx);
-  --divider-width: v-bind(dividerWidthPx);
-  --note-font-size: calc(var(--cell-height) * 0.8);
-  --annotation-font-size: calc(var(--cell-height) * 0.7);
-  /* See OverlaySelect.vue */
-  /* --overlay-select-font-size: calc(var(--cell-height) * 0.6); */
-  --substack-bg: rgba(255, 0, 0, 0.1);
-  --pos-line-width: 1px;
-  --string-width: 1px;
-
-  /* Extra height at the bottom of the stack for tie dragging */
-  --note-container-drag-extender-height: 100px;
-
-  /*https://primevue.org/theming/styled/#colors*/
-  --pos-line-color: var(--p-surface-300);
-  --string-color: var(--p-surface-400);
-
-  --tie-color: var(--p-surface-900);
-  --bend-color: var(--p-surface-900);
-  --tie-dragger-color: var(--p-primary-700);
-
-  --divider-color: var(--p-surface-900);
-  --divider-icon-color: var(--p-surface-50);
-
-  --note-hover-color: var(--p-blue-200);
-  --select-color: var(--p-blue-200);
-  --might-move-color: var(--p-amber-300);
-  --moving-color: var(--p-amber-400);
-  --delete-color: var(--p-red-300);
-
-  --annotation-row-line-color: var(--p-surface-300);
-  --annotation-border: var(--p-surface-300);
-  --annotation-notch-color: var(--p-surface-400);
-  --annotation-dragger-color: var(--p-surface-400);
-  --annotation-dragger-hover-color: var(--p-surface-500);
-  --annotation-hover-background-color: var(--p-blue-200);
-  /* --annotation-default-background-color: var(--p-slate-100); */
-  --annotation-default-background-color: rgb(
-    from var(--annotation-hover-background-color) r g b / 0.4
-  );
-
-  --select-alpha: 0.3;
-
-  --pos-line-z-index: -1;
-  --overlay-svg-z-index: 0;
-  --bar-overlay-z-index: 1;
-  --annotation-dragger-z-index: 1;
-  --annotation-z-index: 2;
-  --divider-z-index: 3;
-  --overlay-controls-z-index: 4;
-  --annotation-current-z-index: 3;
-  --annotation-resize-dragger-z-index: 4;
-  --selection-toolbar-z-index: 4;
-  --note-container-drag-extender-z-index: 5;
-  --tie-dragger-z-index: 5;
-
   user-select: none;
-
   display: flex;
   flex-wrap: wrap;
-}
-
-@media (prefers-color-scheme: dark) {
-  .tab {
-    --pos-line-color: var(--p-surface-600);
-    --string-color: var(--p-surface-700);
-
-    --tie-color: var(--p-surface-300);
-    --bend-color: var(--p-surface-300);
-    --tie-dragger-color: var(--p-primary-600);
-
-    --divider-color: var(--p-primary-600);
-    --divider-icon-color: var(--p-surface-50);
-
-    --note-hover-color: var(--p-blue-100);
-    --select-color: var(--p-blue-200);
-    --might-move-color: var(--p-amber-400);
-    --moving-color: var(--p-amber-500);
-    --delete-color: var(--p-red-400);
-
-    --annotation-row-line-color: var(--p-surface-600);
-    --annotation-border: var(--p-surface-600);
-    --annotation-notch-color: var(--p-surface-500);
-    --annotation-dragger-color: var(--p-surface-400);
-    --annotation-dragger-hover-color: var(--p-surface-300);
-    --annotation-hover-background-color: var(--p-primary-700);
-    /* --annotation-default-background-color: var(--p-slate-700); */
-
-    --annotation-default-background-color: rgb(
-      from var(--p-primary-500) r g b / 0.4
-    );
-  }
 }
 
 .line-break {
